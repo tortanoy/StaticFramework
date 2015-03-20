94604f528b98acf7741c0842afebe17adc8c490a
surapong suwanlee <mr.surapong.s@gmail.com>
2015-03-20 16:26:16 +0700

    add framework

---
+++ StaticFrameworkTest.framework/Versions/Current/Headers/StaticFrameworkTest.h
@@ -0,0 +1,13 @@
+//
+//  StaticFrameworkTest.h
+//  StaticFrameworkTest
+//
+//  Created by iGetWeb on 3/20/2558 BE.
+//  Copyright (c) 2558 iGetWeb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticFrameworkTest : NSObject
+
+@end
